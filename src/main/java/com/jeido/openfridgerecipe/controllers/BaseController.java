cf1174dcbdf40c9f4cb47e9e2080457fbec1eaef
Teo Gontard <teo.gontard@gmail.com>
2024-10-24 11:09:22 +0200

    init

--- src/main/java/com/jeido/openfridgerecipe/controllers/BaseController.java
+++ src/main/java/com/jeido/openfridgerecipe/controllers/BaseController.java
@@ -1,4 +1,16 @@
 package com.jeido.openfridgerecipe.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class BaseController {
+
+    @GetMapping()
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Fridge API Working");
+    }
 }
